793fbce1b04b4384de5d0b3f8706add7590a9ccc
fannybillefalt <fannybillefalt@gmail.com>
2025-10-23 11:16:15 +0200

    Add project files.

---
+++ Labb 1 - OOP/Circle.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Labb_1___OOP
+{
+    internal class Circle
+    {
+        private float _pi = 3.141f;
+        int Radius;
+        public Circle(int radie)
+        {
+            Radius = radie;
+        }
+
+        public float GetArea()
+        {
+            float circleArea = Radius * Radius * _pi;
+            return circleArea;
+        }
+
+        public float Circumference()
+        {
+            float circumference = 2 * _pi * Radius;
+            return circumference;
+        }
+        public float Volume()
+        {
+            float volume = (4 / 3) * _pi * (Radius * Radius * Radius);
+            return volume;
+        }
+        
+    }
+}
---
+++ Labb 1 - OOP/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Labb_1___OOP
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Circle circle1 = new Circle(5);
+            Console.WriteLine($"Arean med radien 5: {circle1.GetArea()}\n");
+            Console.WriteLine($"Omkrets med radien 5: {circle1.Circumference()}\n");
+            Console.WriteLine($"Volymen med radien 5: {circle1.Volume()}\n");
+
+            Circle circle2 = new Circle(6);
+            Console.WriteLine($"Arean med radien 6: {circle2.GetArea()}\n");
+            Console.WriteLine($"Omkrets med radien 6: {circle2.Circumference()}\n");
+            Console.WriteLine($"Volymen med radien 6:{circle2.Volume()}");
+
+
+        }
+    }
+}
